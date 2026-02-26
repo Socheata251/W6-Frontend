5b7fa58e598529d885cfc0c19c12867f161f2abd
Socheata251 <taet3164@gmail.com>
2026-02-26 15:17:14 +0700

    step2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import pnLogo from "./assets/pn-logo.png";
 import Scores from "./components/Scores";
 import Header from "./components/Header";
-import { JAVA_RESULTS, PYTHON_RESULTS, HTML_RESULTS, ENGLISH_RESULTS } from "./data";
+import {  HTML_RESULTS } from "./data";
 
 function App() {
   return (
     <>
       <Header batchName="Fake Batch" />
       <main className="scores-container">
-        <Scores courseName="English" courseResults={ENGLISH_RESULTS} />
+        <Scores courseName="HTML"    courseResults={HTML_RESULTS}    />
       </main>
     </>
   );
